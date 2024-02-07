6ea3f5037f00c3af68a88e001434acc968090ca5
clappd1atwit <clappd1@wit.edu>
2024-02-07 09:33:13 -0500

    Update is_letter_a()

--- src/letters.py
+++ src/letters.py
@@ -1,18 +1,26 @@
 import mediapipe as mp
 from src.helpers import *
 
+# Letter 'A'
+# The letter a is defined by the two following propertes:
+# - The hand(four fingers) is closed
+# - the thumb is to the side of all of the fingers (not across any fingers)
+# returns: true if all the above proprties are true, false otherwise
 def is_letter_a(results, landmarks):
     handedness = is_right_hand(results, landmarks)
-    thumb_tip = landmarks.landmark[mp.solutions.hands.HandLandmark.THUMB_TIP]
-    thumb_mcp = landmarks.landmark[mp.solutions.hands.HandLandmark.THUMB_MCP]
-    index_mcp = landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_MCP]
+    thumb_tip = thumb_tip(landmarks)
+    thumb_mcp = thumb_mcp
+    index_mcp = index_mcp
+    
+    # Right Hand
     if handedness:
-        if not is_hand_open(landmarks) and thumb_tip.y < thumb_mcp.y and thumb_tip.x > index_mcp.x:
+        if is_hand_closed(landmarks) and thumb_tip.y < thumb_mcp.y and thumb_tip.x > index_mcp.x:
             return 'A'
         else:
             return 'Not A'
+    # Left Hand
     else:
-        if not is_hand_open(landmarks) and thumb_tip.y < thumb_mcp.y and thumb_tip.x < index_mcp.x:
+        if is_hand_closed(landmarks) and thumb_tip.y < thumb_mcp.y and thumb_tip.x < index_mcp.x:
             return 'A'
         else:
             return 'Not A'
