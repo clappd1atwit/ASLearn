65af1dbcea6c7d741c267abfdf462113b5ec1d4d
nasrliam <lmojon14@gmail.com>
2024-05-14 12:24:58 -0400

    Gui Edit

--- GUI.py
+++ GUI.py
@@ -1,31 +1,53 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
-class MyGUI(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle('Simple GUI')
-        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle("GUI Landscape")
+        self.setGeometry(90, 90, 600, 400)  # Set window size to full screen
 
-        self.button1 = QPushButton('Tutorials', self)
-        self.button2 = QPushButton('Free Mode Practice', self)
-        self.button3 = QPushButton('Quiz Yourself!', self)
+        # Create central widget and layout
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.button1)
-        layout.addWidget(self.button2)
-        layout.addWidget(self.button3)
+        # Home page buttons
+        self.tutorials_button = QPushButton("Tutorials")
+        self.free_mode_button = QPushButton("Free Mode Practice")
+        self.quiz_button = QPushButton("Quiz")
 
-        self.setLayout(layout)
+        # Connect buttons to respective functions
+        self.tutorials_button.clicked.connect(self.show_tutorials_page)
+        self.free_mode_button.clicked.connect(self.show_free_mode_page)
+        self.quiz_button.clicked.connect(self.show_quiz_page)
 
-def main():
+        # Add buttons to layout
+        self.layout.addWidget(self.tutorials_button)
+        self.layout.addWidget(self.free_mode_button)
+        self.layout.addWidget(self.quiz_button)
+
+    def show_tutorials_page(self):
+        # Replace current widget with Tutorials page
+        tutorials_page = QWidget()
+        tutorials_page.setStyleSheet("background-color: lightblue;")
+        self.setCentralWidget(tutorials_page)
+
+    def show_free_mode_page(self):
+        # Replace current widget with Free Mode Practice page
+        free_mode_page = QWidget()
+        free_mode_page.setStyleSheet("background-color: lightgreen;")
+        self.setCentralWidget(free_mode_page)
+
+    def show_quiz_page(self):
+        # Replace current widget with Quiz page
+        quiz_page = QWidget()
+        quiz_page.setStyleSheet("background-color: lightcoral;")
+        self.setCentralWidget(quiz_page)
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    gui = MyGUI()
-    gui.show()
+    window = MainWindow()
+    window.show()  # Show window in full screen
     sys.exit(app.exec())
-
-if __name__ == '__main__':
-    main()
